978f2ad86e78cc0ed4e5219a14f29c3a8b6379ac
ronen <narkisr@gmail.com>
2018-03-22 20:34:04 +1100

    sh return map

--- src/shim/cljs/shell.cljs
+++ src/shim/cljs/shell.cljs
@@ -1,8 +1,8 @@
 (ns shim.cljs.shell
   (:require-macros  [cljs.core.async.macros :refer  [go]])
-  (:require 
+  (:require
     [cljs.nodejs :as nodejs]
-    [cljs.core.async :as a :refer [put! <! chan alts! timeout]]))
+    [cljs.core.async :as async :refer [put! <! chan alts! timeout take!]]))
 
 (def spawn (.-spawn (js/require "child_process")))
 
@@ -22,18 +22,15 @@
   with the last pair being [:exit code]"
   [cmd & args]
   (let [c (exec-chan cmd (clj->js args))]
-    (go   (loop [output (<! c)               , result []]
-    ;;(go (loop [output (alts! c (timeout 2)), result []]
-            (if (= :exit (first output))
-              (conj result output)
-              (recur (<! c) (conj result output)))))))
+    (go (loop [output (<! c) result []]
+      (if (= :exit (first output))
+        (conj result output)
+        (recur (<! c) (conj result output)))))))
 
 (defn sh [& args]
   (go
-    (let [result (into {} (<! (apply exec args)))]
-       (println result))))
- 
+    (into {} (<! (apply exec args)))))
 
-(comment 
-  (sh "ls" "-la" "/home/ronen")
-  )
+(comment
+  (take! (sh "ls" "-la" "/home/ronen") (fn [r] (println r)))
+  (take!  (fn [r] (println (keys r)))))
