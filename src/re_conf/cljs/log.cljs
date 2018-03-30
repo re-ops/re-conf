1c077e032b2fad37cf2379cbf5d670cd34535812
ronen <narkisr@gmail.com>
2018-03-30 15:14:05 +1100

    adding log ns

---
+++ src/re_conf/cljs/log.cljs
@@ -0,0 +1,30 @@
+(ns re-conf.cljs.log
+  "node logging"
+  )
+
+(def winston (js/require "winston"))
+
+(def settings
+  (let [f (clj->js {"filename" "re-conf.log"})]
+    {
+     :level "info"
+     :transports [
+        (winston.transports.Console.)
+        (winston.transports.File. f)
+      ]
+     }
+   ))
+
+(def logger (.createLogger winston (clj->js settings)))
+
+(defn info
+  "Using winston logging info"
+  [m]
+  (.info logger m))
+
+(comment
+  (info "hello")
+  )
+
+
+
