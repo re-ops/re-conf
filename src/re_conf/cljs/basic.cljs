e8bfa239ff8c4d6a33b608317624f0553d8275f0
ronen <narkisr@gmail.com>
2018-04-04 00:34:15 +1000

    build fix

--- src/re_conf/cljs/basic.cljs
+++ src/re_conf/cljs/basic.cljs
@@ -1,8 +1,7 @@
 (ns re-conf.cljs.basic
-  (:require [re-conf.cljs.core :refer (install checksum -!>)]))
+  (:require [re-conf.cljs.core :refer (install checksum)]))
 
-(defn sanity []
-  (-!> (checksum "/tmp/form-init2576307926037317786.clj" :md5)))
+(defn sanity [])
 
 (comment
   (sanity))
