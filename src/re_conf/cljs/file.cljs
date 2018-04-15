a18d402491c8626e63149980f50d3d961e8c748c
ronen <narkisr@gmail.com>
2018-04-15 22:49:56 +1000

    template resource

--- src/re_conf/cljs/file.cljs
+++ src/re_conf/cljs/file.cljs
@@ -1,12 +1,15 @@
-(ns re-conf.cljs.template
-  "Template resource"
+(ns re-conf.cljs.file
+  "File resources"
   (:require
+   [re-conf.cljs.common :refer (run)]
    [re-conf.cljs.log :refer (info)]
    [cljs.core.async :as async :refer [<! go]]
    [cljs-node-io.core :as io]
    [cljstache.core :refer [render]]))
 
-(defn template
+(def fs (js/require "fs"))
+
+(defn- run-template
   "Create a file from a template with args"
   [args tmpl dest]
   (go
@@ -18,6 +21,17 @@
             {:error {:message "reading template source failed" :error esp :source tmpl}}
             {:ok {:message "writing template source success" :template tmpl :dest dest}}))))))
 
+(defn run-chown 
+   "Change folder owner and group"
+   [dest usr grp]
+  )
+
+(defn template
+  "File template resource"
+  ([args tmpl dest]
+   (template nil args tmpl dest))
+  ([c args tmpl dest]
+   (run c (fn [] (run-template args tmpl dest)))))
+
 (comment
-  (info
-   (template {:keys {:key "abcd" :user "foo@bla"}} "resources/authorized_keys.mustache" "/tmp/keys") ::template))
+  )
