4b0ae3dde59b3a0ac83bb90baed174c9c4a9ef72
ronen <narkisr@gmail.com>
2018-04-27 20:10:57 +1000

    bzip2

--- src/re_conf/cljs/archive.cljs
+++ src/re_conf/cljs/archive.cljs
@@ -2,16 +2,25 @@
   "Archive extraction"
   (:require
    [re-conf.cljs.common :refer (run)]
-   [re-conf.cljs.pkg :refer (install)]
+   [re-conf.cljs.pkg :refer (package)]
    [re-conf.cljs.shell :refer (exec)]))
 
 (def fs (js/require "fs"))
 
 (defn unzip
-  "Unzip a zip file"
+  "Unzip resource"
   [c src dest]
   (if-not (.existsSync fs "/usr/bin/unzip")
     (-> c
-        (install "unzip")
+        (package "unzip")
         (exec "/usr/bin/unzip" "-o" src "-d" dest))
     (exec c "/usr/bin/unzip" "-o" src "-d" dest)))
+
+(defn bzip2
+  "bzip resource"
+  [c dest]
+  (if-not (.existsSync fs "/usr/bin/bzip2")
+    (-> c
+        (package "bzip2")
+        (exec "/bin/bzip2" "-f" "-d" dest))
+    (exec "/bin/bzip2" "-f" "-d" dest)))
