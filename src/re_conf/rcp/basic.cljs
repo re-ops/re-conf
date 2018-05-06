6438d4b65b461bcad70071c74f86012af7b9259e
ronen <narkisr@gmail.com>
2018-05-06 23:56:29 +1000

    recipes moved to re-base

--- src/re_conf/rcp/basic.cljs
+++
@@ -1,8 +0,0 @@
-(ns re-conf.rcp.basic
-  "Basic setup"
-  (:require
-   [re-conf.cljs.download :refer (download checksum)]
-   [re-conf.cljs.archive :refer (unzip)]
-   [re-conf.cljs.shell :refer (exec)]
-   [re-conf.cljs.output :refer (summary)]))
-
