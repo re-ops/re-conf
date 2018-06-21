96c022e58c6865606c075e513688a765b734b76e
ronen <narkisr@gmail.com>
2018-06-21 21:45:01 +1000

    Adding options

--- src/re_conf/resources/user.cljs
+++ src/re_conf/resources/user.cljs
@@ -9,12 +9,19 @@
    [cljs.core.async :refer [<! go]]
    [re-conf.resources.shell :refer (sh)]))
 
+(defn append-options
+   [args opts]
+  (cond->> args
+    (opts :home) (into args ["-m"])))
+
 (defn adduser
   "Add a user"
-  [name]
+  [name options]
   (let [props "'First Last,RoomNumber,WorkPhone,HomePhone'"]
     (go
-      (<! (sh "/usr/sbin/adduser" name "--gecos" props  "--disabled-password")))))
+      (<!
+        (apply sh
+           (append-options ["/usr/sbin/adduser" name "--gecos" props  "--disabled-password"] options))))))
 
 (defn rmuser
   "Remove a user"
@@ -27,12 +34,12 @@
 
 (defn user
   "User management"
-  ([name]
+  ([name options]
    (user nil name :present))
-  ([name state]
-   (user nil name state))
-  ([c name state]
-   (run c #((user-states state) name))))
+  ([name options state]
+   (user nil name options state))
+  ([c name options state]
+   (run c #((user-states state) name options))))
 
 (comment
   (info (user "zfs") :zfs)
