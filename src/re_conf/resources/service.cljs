634f3d5b35c4b23b41dd7590413d96294feaaa41
ronen <narkisr@gmail.com>
2018-07-06 22:32:59 +1000

    adding service

---
+++ src/re_conf/resources/service.cljs
@@ -0,0 +1,60 @@
+(ns re-conf.resources.service
+  "Service handling"
+  (:require-macros
+   [clojure.core.strint :refer (<<)])
+  (:require
+   [re-conf.resources.common :refer (run)]
+   [cljs.core.async :refer [<! go]]
+   [re-conf.resources.log :refer (info debug error channel?)]
+   [re-conf.resources.shell :refer (sh)]))
+
+(defprotocol Service
+  (stop [this service])
+  (start [this service])
+  (restart [this service])
+  (disable [this service]))
+
+(def systemd-bin "/usr/sbin/service")
+
+(defrecord Systemd []
+  Service
+  (start [this service]
+    (debug "starting service" ::systemd)
+    (go
+      (<! (sh systemd-bin "start" service))))
+
+  (stop [this service]
+    (debug "stopping service" ::systemd)
+    (go
+      (<! (sh systemd-bin "stop" service))))
+
+  (restart [this service]
+    (debug "restarting service" ::systemd)
+    (go
+      (<! (sh systemd-bin "restart" service))))
+
+  (disable [this service]
+    (debug "disabling service" ::systemd)
+    (go
+      (<! (sh "/sbin/systemctl" "disable" (<< "~{service}.service"))))))
+
+(def systemd (Systemd.))
+
+(defn into-spec [m args]
+  (if (empty? args)
+    m
+    (let [a (first args)]
+      (cond
+        (string? a) (into-spec (clojure.core/update m :service (fn [v] (conj v a))) (rest args))
+        (channel? a) (into-spec (assoc m :ch a) (rest args))
+        (keyword? a) (into-spec (assoc m :state a) (rest args))
+        (fn? a) (into-spec (assoc m :provider (a)) (rest args))))))
+
+(defn service
+  "Service resource with optional provider and state parameters"
+  ([& args]
+   (let [{:keys [ch service state provider] :or {provider systemd state :present}} (into-spec {} args)
+         fns {:start start :stop stop :disable disable :restart restart}]
+     (if ch
+       (run ch #((fns state) provider service))
+       ((fns state) provider service)))))
