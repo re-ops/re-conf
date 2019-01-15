4f4e5074898f223f3973225676dc63dff0459610
ronen <narkisr@gmail.com>
2019-01-15 22:23:17 +1100

    adding args

--- src/re_conf/resources/common.cljs
+++ src/re_conf/resources/common.cljs
@@ -26,7 +26,7 @@
     (let [start (.hrtime process)
           r (<! (apply f args))
           end (.hrtime process start)]
-      (debug (assoc r :profile end :function (function-name f)) ::profile)
+      (debug (assoc r :profile end :function (function-name f) :args args) ::profile)
       r)))
 
 (defn run [c f args]
