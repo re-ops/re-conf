deac42fb2c210dcb179d1d49b84340dbf080c535
ronen <narkisr@gmail.com>
2019-01-15 22:22:54 +1100

    cleanup

--- src/re_conf/main.cljs
+++ src/re_conf/main.cljs
@@ -3,7 +3,6 @@
    [re-conf.resources.common :refer (profile function-name)]))
 
 (defn -main [& args]
-  (println (str identity))
-  (println (function-name identity)))
+  )
 
 (set! *main-cli-fn* -main)
